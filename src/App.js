47b8df99abaee3c7e61e3c0b5af3e2d51c8ed8b5
jeganpetchimuthu <127992705+Jeganpetchimuthu@users.noreply.github.com>
2023-06-16 20:59:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,292 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <div className="flex">
+      <div className="cantainer">
+        <div className="header-cantainer">
+          <h6 className="free">FREE</h6>
+          <h1 className="free">$0</h1>
+          <h6 className="month">/month</h6>
+        </div>
+        <div className="span">
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <div className="value">
+            <span className="sub">Single User</span>
+            <br />
+            <span className="sub">5GB Storage</span>
+            <br />
+            <span className="sub">Unlimited Public Projects</span>
+            <br />
+            <span className="sub">Community Access</span>
+            <br />
+            <span className="sub">Unlimited Private Projects</span>
+            <br />
+            <span className="sub">Dedicated Phone Support</span>
+            <br />
+            <span className="sub">Free Subdomain</span>
+            <br />
+            <span className="sub">Monthly Status Reports</span>
+            <br />
+          </div>
+        </div>
+        <button className="submit" type="submit">
+          BUTTON
+        </button>
+      </div>
+      <div className="cantainer">
+        <div className="header-cantainer">
+          <h6 className="free">PLUS</h6>
+          <h1 className="free">$9</h1>
+          <h6 className="month">/month</h6>
+        </div>
+        <div className="span">
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <div className="value">
+            <span className="sub">5 Users</span>
+            <br />
+            <span className="sub">50GB Storage</span>
+            <br />
+            <span className="sub"> Unlimited Public Projects</span>
+            <br />
+            <span className="sub">Community Access</span>
+            <br />
+            <span className="sub"> Unlimited Private Projects</span>
+            <br />
+            <span className="sub">Dedicated Phone Support</span>
+            <br />
+            <span className="sub">Free Subdomain</span>
+            <br />
+            <span className="sub">Monthly Status Reports</span>
+            <br />
+          </div>
+        </div>
+        <button className="submit" type="submit">
+          BUTTON
+        </button>
+      </div>
+      <div className="cantainer">
+        <div className="header-cantainer">
+          <h6 className="free">pro</h6>
+          <h1 className="free">$49</h1>
+          <h6 className="month">/month</h6>
+        </div>
+        <div className="span">
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <input
+            type="checkbox"
+            name="unkmark"
+            value="unkmark"
+            tabindex="1"
+            checked="checked"
+          />
+          <br />
+          <div className="value">
+            <span className="sub">Unlimited Users</span>
+            <br />
+            <span className="sub">150GB Storage</span>
+            <br />
+            <span className="sub">Unlimited Public Projects</span>
+            <br />
+            <span className="sub">Community Access</span>
+            <br />
+            <span className="sub">Unlimited Private Projects</span>
+            <br />
+            <span className="sub">Dedicated Phone Support</span>
+            <br />
+            <span className="sub">Unlimited Free Subdomains</span>
+            <br />
+            <span className="sub">Monthly Status Reports</span>
+            <br />
+          </div>
+        </div>
+        <button className="submit" type="submit">
+          BUTTON
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
